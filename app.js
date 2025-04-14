f18f8c2dcde9de72bdc8f57395a2d64ed72b0026
PV-Lopes <pedroca.vitor@outlook.com>
2025-04-13 22:51:33 -0300

    Adição do codigo node

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-app.use('/public', express.static('public'));
+const express = require('express');
+const app = express();
+const port = process.env.port || 3000;
+
+app.use(express.static('puplic'));
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + index.html);
+}
+)
+
+app.listen(port, () =>{
+    console.log("Servidor rodando")
+})
